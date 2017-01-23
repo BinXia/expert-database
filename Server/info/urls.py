fe72e10e8dd372c553d31bde7c5b7aec73422b94
Kryolith <ben.binxia@gmail.com>
2017-01-23 19:30:35 +0800

    Init the project with basic server and theme.

---
+++ Server/info/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from info import views
+
+urlpatterns = [
+    # url(r'^Dynamic', views.UpdataDynamicData, name='UpdataDynamicData'),
+    # url(r'^Search', views.SearchForHistory, name='SearchForHistory'),
+    # url(r'^HistoryRecord', views.RecordForHistory, name='RecordForHistory'),
+
+]
---
+++ Websites/js/common.js
@@ -0,0 +1,2 @@
+var host = "http://0.0.0.0";
+var port = "8080";
